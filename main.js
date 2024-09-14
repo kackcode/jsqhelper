ca62610a3326eac6877d1cf84d8e62e58446695c
undefined <undefined@undefined.local>
2024-09-14 23:01:21 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,239 @@
+SERVER_ERROR = 'Server Error '
+
+const getTagValue = (element) => {
+    return  element.target.dataset.id
+}
+
+const getDataValue = (element, attribute) => {
+    return $(element).data(attribute)
+}
+
+const toastNotification = (type, message) => {
+    const notyf = new Notyf({
+        position: {
+            x: 'right',
+            y: 'top',
+        },
+        duration: 9000,
+        icon: false,
+        dismissible:true
+    });
+    if (type === 'success') {
+        notyf.success({
+            message: message
+        });
+    } else if (type === 'error') {
+        notyf.error({
+            message: message
+        });
+    }
+};
+
+function formToJson($form, except = null) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function (n, i) {
+        indexed_array[n['name']] = n['value'];
+    });
+
+    if (except) {
+        $.map(except, function (e) {
+            delete indexed_array[e]
+        })
+    }
+    return JSON.stringify(indexed_array);
+}
+
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+
+function ajaxRequest(data, method, url, callback,notificationHandler = toastNotification) {
+    $.ajax({
+        type: method,
+        url: url,
+        data: data,
+        dataType: "JSON",
+        contentType: "application/json; charset=utf-8",
+        success: function (response) {
+            callback(response)
+        },
+        error: function (response) {
+            if (response.status != 200 | 201) {
+                if (response.status == 400) {
+                    errorResponse = response.responseJSON
+                    if (errorResponse.message) {
+                        notificationHandler('error',errorResponse.message);
+                        return false
+                    }
+                } else if (response.status == 500) {
+                    notificationHandler('error',SERVER_ERROR);
+                }
+            } else {
+                if (callback != null) {
+                    callback(response)
+                }
+            }
+        }
+    });
+}
+
+
+function handlerGlobalAjaxError() {
+    $(document).ajaxError(function (event, jqxhr, settings, thrownError) {
+        jsonError = jqxhr.responseJSON
+        if (jqxhr.status != 200) {
+            if (jqxhr.status == 400) {
+                console.log("validation errors cache from globalAjax Error Handler")
+            } else if (jqxhr.status == 500) {
+                swal("Erreur !", SERVER_ERROR, "error");
+            }
+        }
+
+    });
+}
+
+function spinnerShow(submitBtn, spinnerBtn, labelBtn, labelText) {
+    submitBtn.attr('disabled', true);
+    spinnerBtn.addClass('spinner-border');
+    labelBtn.text(labelText);
+}
+
+function spinnerHide(submitBtn, spinnerBtn, labelBtn, labelText) {
+    submitBtn.attr('disabled', false);
+    spinnerBtn.removeClass('spinner-border');
+    labelBtn.text(labelText);
+}
+
+
+function ajaxUploadRequest(data, method, url, callback,beforCallback=null,completedCallback = null,notificationHandler = toastNotification) {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        cache: false,
+        // contentType: "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" ,
+        contentType: false,
+        // mimeType: 'multipart/form-data', ;boundary=gc0p4Jq0M2Yt08jU534c0p
+        processData: false,
+        data: data,
+        type: method,
+        success: function (response) {
+            callback(response)
+        },
+        error: function (response) {
+            if (response.status != 200) {
+                if (response.status == 400) {
+                    errorResponse = response.responseJSON
+                    if (errorResponse.message) {
+                        notificationHandler('error',errorResponse.message);
+                        if (errorResponse.data) {
+                        }
+                        return false
+                    }
+                }
+            } else {
+                if (callback != null) {
+                    callback(response)
+                }
+            }
+        },
+        beforeSend: function () {
+            if (beforCallback != null) {
+                beforCallback()
+            }
+        },
+        complete: function () {
+            if (completedCallback != null) {
+                completedCallback()
+            }
+        }
+    });
+}
+
+
+function response(response) {
+    if (response.error) {
+        swal("Failure !", response.message, "error");
+    } else {
+        swal("Successful", response.message, "success")
+            .then((value) => {
+                location.reload()
+            });
+
+    }
+}
+
+function responseSwal(response) {
+    const swalType = response.error ? "error" : "success";
+    const swalTitle = response.error ? "Failure!" : "Successful";
+    swal(swalTitle, response.message, swalType);
+}
+
+
+function responsewithData(response) {
+    if (response.error) {
+        swal("Failure !", response.message, "error");
+    } else {
+        swal("Successful", response.message, "success")
+            .then((value) => {
+                location.replace(response.data.url)
+            });
+    }
+}
+
+function swalAlert(title, text, obj, data, methode, link, response) {
+    $this = obj
+    swal({
+            title: title,
+            text: text,
+            icon: "warning",
+            buttons: {
+                cancel: 'NO',
+                catch: {
+                    text: 'YES',
+                    value: 'confirm'
+                }
+            },
+            dangerMode: true,
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                ajaxRequest(data, methode, link, response)
+                $($this.parent().parent()).remove();
+            } else {
+                swal("ACTION CANCELED");
+            }
+        });
+}
+
+function getDataIPInfo() {
+    var dataArray = [];
+
+    $.ajax({
+        url: "https://ipinfo.io/json",
+        type: "GET",
+        success: function(response) {
+            dataArray = (response);
+        },
+        error: function(xhr, status, error) {
+            // console.error("Error:", error);
+        },
+        async: false
+    });
+    delete dataArray['readme'];
+    return dataArray;
+}
+
+
+function processData(data) {
+    data = JSON.parse(data);
+    data['geolocalisation'] = getDataIPInfo();
+    data = JSON.stringify(data);
+    return data;
+
+}
